b68db0dc6ac319307ad1f4792b87ae684f1653a8
CosmicArcher <65287527+CosmicArcher@users.noreply.github.com>
2020-12-01 18:14:00 +0800

    Bullet shooting and weapon switching implemented, bullets do not have any distinguishing features yet

---
+++ KappaDayOff/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    protected Vector3 destination;
+    protected Vector3 flightDirection;
+    protected float speed = 0.25f;
+    protected int damage = 10;
+
+    public void SetBulletDestination(Vector3 target)
+    {
+        destination = target;
+        flightDirection = (target - gameObject.transform.position).normalized;
+    }
+
+    private void Update()
+    {
+        gameObject.transform.position += speed * flightDirection;
+    }
+}
---
+++ KappaDayOff/Assets/Scripts/ShooterBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterBehavior : MonoBehaviour
+{
+    public GameObject bulletOriginPoint;
+    public GameObject crosshair;
+    public GameObject[] bulletPrefabs;
+
+    private int chosenBulletIndex = 0;
+
+    public void SwitchGunLeft()
+    {
+        if (chosenBulletIndex > 0)
+            chosenBulletIndex--;
+        else
+            chosenBulletIndex = bulletPrefabs.Length - 1;
+    }
+
+    public void SwitchGunRight()
+    {
+        if (chosenBulletIndex < bulletPrefabs.Length - 1)
+            chosenBulletIndex++;
+        else
+            chosenBulletIndex = 0;
+    }
+
+    public void Shoot()
+    {
+        GameObject newBullet = Instantiate(bulletPrefabs[chosenBulletIndex], bulletOriginPoint.transform.position, Quaternion.identity);
+        BulletBehavior bullet = newBullet.GetComponent<BulletBehavior>();
+        if (bullet != null)
+            bullet.SetBulletDestination(crosshair.transform.position);
+    }
+}
